8941565d344e9db3f9facb1083bf01386cc60ecd
Art <d.art.shishkin@gmail.com>
2022-02-06 10:44:12 +0200

    25. Cross Component Communication with Property Binding (#2)

--- ng-refresher/src/app/app.component.ts
+++ ng-refresher/src/app/app.component.ts
@@ -7,4 +7,6 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-refresher';
+
+  persons = ['Art', 'Kate', 'Arina', 'Nazar'];
 }
--- ng-refresher/src/app/persons/persons.component.ts
+++ ng-refresher/src/app/persons/persons.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-persons',
@@ -7,7 +7,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PersonsComponent implements OnInit {
 
-  constructor() { }
+  @Input() personList: string[];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
