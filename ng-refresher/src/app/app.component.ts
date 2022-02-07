7ee3c0bba286641b4ac76d357f0915de846bd959
Art <d.art.shishkin@gmail.com>
2022-02-07 11:15:14 +0200

    33. Setting Up Services (#2)

--- ng-refresher/src/app/app.component.ts
+++ ng-refresher/src/app/app.component.ts
@@ -6,11 +6,4 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-refresher';
-
-  persons = ['Art', 'Kate', 'Arina', 'Nazar'];
-
-  onPersonCreated(name: string) {
-    this.persons.push(name);
-  }
 }
---
+++ ng-refresher/src/app/persons/persons.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonsService {
+
+  persons = ['Art', 'Kate', 'Arina', 'Nazar'];
+
+  constructor() {
+  }
+
+  addPerson(name: string) {
+    this.persons.push(name);
+  }
+}
