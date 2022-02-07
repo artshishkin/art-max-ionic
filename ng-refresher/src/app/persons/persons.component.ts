4d522d03c33c4fbb835e6f8f27392a80906c6587
Art <d.art.shishkin@gmail.com>
2022-02-07 12:50:38 +0200

    34. Using Services with Dependency Injection (#2)

--- ng-refresher/src/app/persons/persons.component.ts
+++ ng-refresher/src/app/persons/persons.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PersonsService} from "./persons.service";
 
 @Component({
   selector: 'app-persons',
@@ -7,9 +8,10 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class PersonsComponent implements OnInit {
 
-  @Input() personList: string[];
+  personList: string[];
 
-  constructor() {
+  constructor(private personsService: PersonsService) {
+    this.personList = this.personsService.persons;
   }
 
   ngOnInit(): void {
