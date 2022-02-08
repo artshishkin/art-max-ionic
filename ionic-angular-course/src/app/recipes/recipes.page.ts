a059e65f4dd26d4664bc712fab130024bbbb2f69
Art <d.art.shishkin@gmail.com>
2022-02-08 21:39:06 +0200

    74. Using Angular Features on Ionic Components (#4)

--- ionic-angular-course/src/app/recipes/recipes.page.ts
+++ ionic-angular-course/src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,7 +8,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RecipesPage implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Hot dog',
+      imageUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/sticky-cider-onion-hot-dogs-24168bf.jpg' +
+        '?quality=90&webp=true&resize=375,341',
+      ingredients: ['butter', 'sausages', 'hot dog buns', '2 tbsp mayonnaise', '1 tbsp olive oil', '2 finely sliced onions']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti puttanesca',
+      imageUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/puttanesca-cfb4e42.jpg' +
+        '?quality=90&webp=true&resize=375,341',
+      ingredients: [
+        '3 tbsp olive oil',
+        '1 onion, finely chopped',
+        '2 large garlic cloves, crushed',
+        '400g can chopped tomatoes',
+        '5 anchovy fillets, finely chopped',
+        '120g pitted black olives',
+        '2 tbsp capers, drained',
+        '300g dried spaghetti',
+        '½ small bunch of parsley, finely chopped'
+      ]
+    }
+
+
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
