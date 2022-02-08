408984b34f5ee22eb1a16cf4772c275b7b261c5d
Art <d.art.shishkin@gmail.com>
2022-02-08 10:48:23 +0200

    59. Finishing Up the Base JavaScript Logic (#3)

--- ionic-non-angular/app.js
+++ ionic-non-angular/app.js
@@ -3,6 +3,14 @@ const amountInput = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
+
+let totalExpenses = 0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
 
 confirmBtn.addEventListener('click', () => {
 
@@ -18,6 +26,9 @@ confirmBtn.addEventListener('click', () => {
 
     expensesList.appendChild(newItem);
 
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+    clear();
 });
 
-
+cancelBtn.addEventListener('click', clear);
