e5126c240195a1268387a8da99948ac2439e2805
Art <d.art.shishkin@gmail.com>
2022-02-08 22:38:52 +0200

    77. Extracting and Displaying Route Param Data (#4)

--- ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {RecipesService} from '../recipes.service';
+import {map} from "rxjs/operators";
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +12,21 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RecipeDetailPage implements OnInit {
 
-  constructor() { }
+  recipe: Recipe;
+
+  constructor(private activatedRoute: ActivatedRoute, private recipesService: RecipesService) {
+  }
 
   ngOnInit() {
+    this.activatedRoute.paramMap
+      .pipe(
+        map(params => params.get('recipeId')),
+        map(recipeId => this.recipesService.getRecipe(recipeId))
+      )
+      .subscribe(recipe => {
+        console.log(recipe);
+        this.recipe = recipe;
+      });
   }
 
 }
