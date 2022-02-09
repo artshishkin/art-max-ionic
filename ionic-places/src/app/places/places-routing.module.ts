4dbaa7a94a6a44a468e2c6a393990fc458f407f6
Art <d.art.shishkin@gmail.com>
2022-02-09 21:48:50 +0200

    111.1 Adding Tabs to the App (#7)

--- ionic-places/src/app/places/places-routing.module.ts
+++ ionic-places/src/app/places/places-routing.module.ts
@@ -6,20 +6,28 @@ import {PlacesPage} from './places.page';
 const routes: Routes = [
   {
     path: '',
-    component: PlacesPage
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+      },
+      {
+        path: 'offers',
+        loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'discover',
+        pathMatch: 'full'
+      }
+    ]
   },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PlacesPageRoutingModule {}
+export class PlacesPageRoutingModule {
+}
