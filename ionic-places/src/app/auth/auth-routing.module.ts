06164fbf951c8116a343a2bd9fe71d0a4d596a32
Art <d.art.shishkin@gmail.com>
2022-02-09 20:34:59 +0200

    107. Creating Our App Pages (#7)

--- ionic-places/src/app/auth/auth-routing.module.ts
+++ ionic-places/src/app/auth/auth-routing.module.ts
@@ -1,16 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomePage} from './home.page';
+
+import {AuthPage} from './auth.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage,
+    component: AuthPage
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class AuthPageRoutingModule {}
--- ionic-places/src/app/places/offers/offers.page.spec.ts
+++ ionic-places/src/app/places/offers/offers.page.spec.ts
@@ -1,19 +1,19 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {IonicModule} from '@ionic/angular';
 
-import {HomePage} from './home.page';
+import {OffersPage} from './offers.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('OffersPage', () => {
+  let component: OffersPage;
+  let fixture: ComponentFixture<OffersPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ OffersPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(OffersPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
