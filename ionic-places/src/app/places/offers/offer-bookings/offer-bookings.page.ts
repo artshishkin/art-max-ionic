c3e384538794f0bab4bdaefe5394d6ca2210a99a
Art <d.art.shishkin@gmail.com>
2022-02-10 12:22:32 +0200

    Assignment 2: Practicing Ionic + Angular (#7)

--- ionic-places/src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ ionic-places/src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,9 +10,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class OfferBookingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
   }
 
+  onClickBack() {
+    this.navCtrl.navigateBack(['..'], {relativeTo: this.activatedRoute}).then();
+  }
 }
