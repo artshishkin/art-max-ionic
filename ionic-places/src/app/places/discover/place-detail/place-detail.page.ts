4db2366fbad105178c5e05eda303ba3fcd907af6
Art <d.art.shishkin@gmail.com>
2022-02-10 11:24:00 +0200

    115. Going Back with NavController (#7)

--- ionic-places/src/app/places/discover/place-detail/place-detail.page.ts
+++ ionic-places/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +10,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
   }
 
+  onBookPlace() {
+    // this.router.navigate(['..']).then();
+    this.navCtrl.navigateBack(['..'], {relativeTo: this.route}).then();
+    // this.navCtrl.pop().then();
+  }
 }
