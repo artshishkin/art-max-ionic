8a459715c94a36add856967c7bc8bf1e982a3cd3
Art <d.art.shishkin@gmail.com>
2022-02-11 10:45:59 +0200

    125. Opening a Modal (#7)

--- ionic-places/src/app/places/discover/place-detail/place-detail.page.ts
+++ ionic-places/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
-import {NavController} from '@ionic/angular';
+import {ModalController, NavController} from '@ionic/angular';
 import {map} from 'rxjs/operators';
 
 import {PlacesService} from '../../places.service';
 import {Place} from '../../place.model';
+import {CreateBookingComponent} from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -20,7 +21,8 @@ export class PlaceDetailPage implements OnInit {
     private router: Router,
     private navCtrl: NavController,
     private route: ActivatedRoute,
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
   ) {
   }
 
@@ -36,7 +38,8 @@ export class PlaceDetailPage implements OnInit {
 
   onBookPlace() {
     // this.router.navigate(['..']).then();
-    this.navCtrl.navigateBack(['..'], {relativeTo: this.route}).then();
+    // this.navCtrl.navigateBack(['..'], {relativeTo: this.route}).then();
     // this.navCtrl.pop().then();
+    this.modalCtrl.create({component: CreateBookingComponent}).then(modalEl => modalEl.present());
   }
 }
