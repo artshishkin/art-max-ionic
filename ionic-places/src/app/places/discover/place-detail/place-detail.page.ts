5ad8681f79f756bdc171c95686fcc901037be997
Art <d.art.shishkin@gmail.com>
2022-02-15 11:31:29 +0200

    148. Using the ActionSheet Controller (#8)

--- ionic-places/src/app/places/discover/place-detail/place-detail.page.ts
+++ ionic-places/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 
-import {ModalController, NavController} from '@ionic/angular';
+import {ActionSheetController, ModalController, NavController} from '@ionic/angular';
 import {map} from 'rxjs/operators';
 
 import {PlacesService} from '../../places.service';
@@ -22,7 +22,8 @@ export class PlaceDetailPage implements OnInit {
     private navCtrl: NavController,
     private route: ActivatedRoute,
     private placesService: PlacesService,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private actionSheetCtrl: ActionSheetController
   ) {
   }
 
@@ -40,6 +41,35 @@ export class PlaceDetailPage implements OnInit {
     // this.router.navigate(['..']).then();
     // this.navCtrl.navigateBack(['..'], {relativeTo: this.route}).then();
     // this.navCtrl.pop().then();
+    this.actionSheetCtrl
+      .create({
+        header: 'Choose an action',
+        buttons: [
+          {
+            text: 'Select Date',
+            handler: () => {
+              this.openBookingModal('select');
+            }
+          },
+          {
+            text: 'Random Date',
+            handler: () => {
+              this.openBookingModal('random');
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          }
+        ]
+      })
+      .then(actionSheetEl => {
+        actionSheetEl.present();
+      });
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: {selectedPlace: this.place}
