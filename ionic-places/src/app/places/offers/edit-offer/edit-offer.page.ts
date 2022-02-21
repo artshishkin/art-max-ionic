614c02655e83542fdbca5ff5e2c67ee2f07a3bc0
Art <d.art.shishkin@gmail.com>
2022-02-21 16:07:08 +0200

    177. Edit Form Challenge (#11)

--- ionic-places/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ ionic-places/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {map} from 'rxjs/operators';
 
 import {PlacesService} from '../../places.service';
@@ -13,19 +14,36 @@ import {Place} from '../../place.model';
 export class EditOfferPage implements OnInit {
 
   place: Place;
+  form: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    private placesService: PlacesService) {
+    private placesService: PlacesService,
+    private builder: FormBuilder) {
   }
 
   ngOnInit() {
     this.route.paramMap
       .pipe(
         map(params => params.get('placeId')),
-        map(placeId=>this.placesService.getById(placeId))
+        map(placeId => this.placesService.getById(placeId))
       )
-      .subscribe(place => this.place = place);
+      .subscribe(place => {
+        this.place = place;
+        this.form = this.builder.group({
+          title: this.builder.control(place.title, {
+            updateOn: 'blur',
+            validators: [Validators.required]
+          }),
+          description: this.builder.control(place.description, {
+            updateOn: 'blur',
+            validators: [Validators.required, Validators.maxLength(180)]
+          })
+        });
+      });
   }
 
+  onEditOffer() {
+    console.log(this.form.value);
+  }
 }
