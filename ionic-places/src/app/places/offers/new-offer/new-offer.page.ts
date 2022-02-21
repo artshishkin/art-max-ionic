75b1c43359c35a91350d531c09bd8c7ee4868769
Art <d.art.shishkin@gmail.com>
2022-02-21 15:28:31 +0200

    174. Creating a Reactive Form (#11)

--- ionic-places/src/app/places/offers/new-offer/new-offer.page.ts
+++ ionic-places/src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -7,10 +8,35 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NewOfferPage implements OnInit {
 
-  constructor() {
+  form: FormGroup;
+
+  constructor(private builder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      title: this.builder.control(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: this.builder.control(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      price: this.builder.control(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      dateFrom: this.builder.control(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      dateTo: this.builder.control(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
+
   }
 
   onCreateOffer() {
